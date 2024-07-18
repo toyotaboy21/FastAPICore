d680048f6ff145975ea752e85be07db857678989
Reques6e <voronin99borja61275y@rambler.ru>
2024-07-18 14:35:54 +0300

    ✨ Новая типизация для базы данных

---
+++ fastcore/core/typedata.py
@@ -0,0 +1,15 @@
+# Типизиация
+#
+# Создал для SQL запросов, т.к некоторых типов данных которые есть
+# в SQL нету в Python, к примеру, BIGINTEGER.
+#
+# Reques6e 2024
+
+
+from typing import NewType
+
+BigInt = NewType('BigInt', int)
+Int = NewType('Int', int)
+Float = NewType('Float', float)
+Bool = NewType('Bool', bool)
+Str = NewType('Str', str)

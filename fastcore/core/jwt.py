ec75718916c7bf6811f9b50eabb169926ad8572a
Reques6e <voronin99borja61275y@rambler.ru>
2024-07-18 14:35:42 +0300

    ✨ Добавляю jwt

---
+++ fastcore/core/jwt.py
@@ -0,0 +1,19 @@
+# JWT 
+#
+# Труебуется для шифрования/дешифрования str. 
+#
+#
+# Reques6e 2024
+
+
+from fastcore.core.exceptions import JWTKeyError
+
+class JWT:
+    def __init__(self) -> None:
+        pass
+
+    async def crypted() -> None:
+        ...
+
+    async def encrypted() -> None:
+        ...

1468d0f093f43a7864938f9c677f6ba6c750e73b
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-07-23 22:05:44 +0300

    Добавил кастомный хендлер FastCoreExceptionHandlers

---
+++ fastcore/core/http.py
@@ -0,0 +1,45 @@
+from fastapi import Request, FastAPI
+from fastapi.responses import JSONResponse
+from fastcore.core.response import JSONBuildResponse
+
+class FastCoreExceptionHandlers:
+    def __init__(self, app: FastAPI):
+        self.app = app
+        self.register_handlers()
+
+    def register_handlers(self):
+        @self.app.exception_handler(404)
+        async def custom_404_handler(request: Request, exc):
+            return JSONResponse(
+                content=JSONBuildResponse(
+                    error=1,
+                    message="Страница не найдена"
+                ).json()
+            )
+
+        @self.app.exception_handler(403)
+        async def custom_403_handler(request: Request, exc):
+            return JSONResponse(
+                content=JSONBuildResponse(
+                    error=1,
+                    message="У вас нет доступа к этой странице"
+                ).json()
+            )
+        
+        @self.app.exception_handler(401)
+        async def custom_401_handler(request: Request, exc):
+            return JSONResponse(
+                content=JSONBuildResponse(
+                    error=1,
+                    message="Требуется авторизация"
+                ).json()
+            )
+
+        @self.app.exception_handler(500)
+        async def custom_500_handler(request: Request, exc):
+            return JSONResponse(
+                content=JSONBuildResponse(
+                    error=1,
+                    message="На сервере произошла ошибка"
+                ).json()
+            )

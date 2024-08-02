fc3bac82e87a5e960ce7631767e99bb6a950ee05
reques6e <voronin99borja61275y@rambler.ru>
2024-08-02 09:10:57 +0300

    ✨ Добавил setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="fastcore",
+    version="0.0.1 Alpha 1",
+    packages=find_packages(),
+    install_requires=install_requires,
+    author="Reques6e",
+    author_email="lygitormaa@gmail.com",
+    description="FastCore",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_python_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
